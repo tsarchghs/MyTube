38b31bb12c46dbfbcf42dcd693decfec057b345f
gjergjk71 <gjergjk71@gmail.com>
2018-07-21 23:34:44 +0200

    Created Video model

--- video/models.py
+++ video/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from channels.models import Channel
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Video(models.Model):
+	channel = models.OneToOneField(Channel,on_delete=models.CASCADE)
+	video_file = models.FileField(upload_to="videos")
+	title = models.CharField(max_length=300)
+	description = models.TextField()
