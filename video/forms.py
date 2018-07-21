b62aee53e1db8ae42b5d5cc674e3c8fc1b950814
gjergjk71 <gjergjk71@gmail.com>
2018-07-22 00:43:28 +0200

    Fixed bug on VideoForm

--- video/forms.py
+++ video/forms.py
@@ -1,8 +1,7 @@
 from django import forms
 from .models import *
 
-class VideoForm(forms.Form):
+class VideoForm(forms.ModelForm):
 	class Meta:
 		model = Video
-		#fields generates a list of all editable fields of Student model
-		fields = [f.name for f in Video._meta.get_fields() if not f.name in ["created","modified"]]
+		exclude = ["channel"]
