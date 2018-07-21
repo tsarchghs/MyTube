d15e4933cf4217bb37c1992ded3b7d2338f4d555
grali <graligralih@gmail,comssh-keygen -t rsa -C YOUR@EMAIL.comgit config --global user.email graligralih@gmail,com>
2018-07-21 22:13:46 +0100

    Finished Backend of Channels

--- channels/models.py
+++ channels/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Channel(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+
+
+
+    def __str__(self):
+        return "{user}, created channel: '{name}'".format(name=self.name, user=self.user)
+
+    def __unicode__(self):
+        return "{user}, created channel: '{name}'".format(name=self.name, user=self.user)
