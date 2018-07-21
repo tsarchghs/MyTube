2fcdbb7ccce5a20ebfc429364d26a292d447bbf0
gjergjk71 <gjergjk71@gmail.com>
2018-07-21 23:39:16 +0200

    Added fields created,modified to all models

--- video/models.py
+++ video/models.py
@@ -5,18 +5,24 @@ from django.contrib.auth.models import User
 
 class Video(models.Model):
 	channel = models.OneToOneField(Channel,on_delete=models.CASCADE)
+	created = models.DateTimeField(auto_now_add=True)
+	modified = models.DateTimeField(auto_now=True)
 	video_file = models.FileField(upload_to="videos")
 	title = models.CharField(max_length=300)
 	description = models.TextField()
 
 class Like(models.Model):
 	user = models.ForeignKey(User,on_delete=models.CASCADE)
+	created = models.DateTimeField(auto_now_add=True)
+	modified = models.DateTimeField(auto_now=True)
 	video = models.ForeignKey(Video,on_delete=models.CASCADE)
 	like = models.BooleanField()
 	dislike = models.BooleanField()
 
 class Comment(models.Model):
 	user = models.ForeignKey(User,on_delete=models.CASCADE)
+	created = models.DateTimeField(auto_now_add=True)
+	modified = models.DateTimeField(auto_now=True)
 	video = models.ForeignKey(Video,on_delete=models.CASCADE)
 	content = models.TextField()
 
