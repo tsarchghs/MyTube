5ec046ec06657e11d620065f44e8140c9c99ac46
gjergjk71 <gjergjk71@gmail.com>
2018-07-22 02:59:42 +0200

    Created editVideo path

--- video/urls.py
+++ video/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path("create",views.createVideo,name="createVideo")
+	path("create",views.createVideo,name="createVideo"),
+	path("edit/<video_id>",views.editVideo,name="editVideo"),
 ]
