e90ae92c85571d6a7fad5c156ce41d1cc50bc344
gjergjk71 <gjergjk71@gmail.com>
2018-07-22 03:36:57 +0200

    Created showVideo view

--- video/views.py
+++ video/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render,redirect,reverse
 from channels.models import Channel
-from .models import Video
+from .models import Video,VideoLike,Comment,CommentLike
 from .forms import VideoForm
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
+from collections import OrderedDict
 # Create your views here.
+
+def showVideo(request,video_id):
+	video = Video.objects.get(pk=video_id)
+	video_likes = len(VideoLike.objects.filter(video=video,like=True,dislike=False))
+	video_dislikes = len(VideoLike.objects.filter(video=video,like=False,dislike=True))
+	video_comments = Comment.objects.filter(video=video).order_by("-id")
+	comment_likes = OrderedDict()
+	for comment in video_comments:
+		likes = CommentLike.objects.filter(comment=comment,like=True,dislike=False)
+		dislikes = CommentLike.objects.filter(comment=comment,like=False,dislike=True)		
+		comment_likes[comment] = len(likes) - len(dislikes)
+	context = {"video":video,
+			   "video_likes":video_likes-video_dislikes,
+			   "comment_likes":comment_likes,			   
+			   }
+	return render(request,"video/showMovie.html",context)
+
 
 @login_required
 def createVideo(request):
