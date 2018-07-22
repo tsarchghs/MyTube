de0fcf88aa7d08dfd32276d2ea48e70154a96e80
gjergjk71 <gjergjk71@gmail.com>
2018-07-22 03:04:35 +0200

    Created editVideo view

--- video/views.py
+++ video/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,redirect,reverse
 from channels.models import Channel
+from .models import Video
 from .forms import VideoForm
 from django.contrib.auth.decorators import login_required
-
+from django.http import Http404
 # Create your views here.
 
 @login_required
@@ -25,3 +26,22 @@ def createVideo(request):
 			return render(request,"video/createVideo.html",{"form":form})
 	else:
 		pass #show you don't have a channel page
+
+@login_required
+def editVideo(request,video_id):
+	current_user = request.user
+	video = Video.objects.get(pk=video_id)
+	if video.channel.user == current_user:
+		if request.method == "POST":
+			form = VideoForm(request.POST,request.FILES,instance=video)
+			if form.is_valid():
+				video = form.save(commit=False)
+				video.save()
+				return redirect(reverse("editVideo",args=[video_id]))
+			else:
+				return render(request,"video/createVideo.html",{"form":form})
+		else:
+			form = VideoForm(instance=video)
+			return render(request,"video/createVideo.html",{"form":form})
+	else:
+		raise Http404
