5538aabac8ab553f4049ff726e19cc0aabf1342f
gjergjk71 <gjergjk71@gmail.com>
2018-07-22 03:37:14 +0200

    Created showVideo path

--- video/urls.py
+++ video/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	path("<video_id>",views.showVideo,name="showVideo"),
 	path("create",views.createVideo,name="createVideo"),
 	path("edit/<video_id>",views.editVideo,name="editVideo"),
 	path("delete/<video_id>",views.deleteVideo,name="deleteVideo"),
