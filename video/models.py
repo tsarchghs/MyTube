2ee458ba6cf5bbc7d51ef9df6fe67fd118d403b4
gjergjk71 <gjergjk71@gmail.com>
2018-07-22 02:59:13 +0200

    Removed "." from extensions

--- video/models.py
+++ video/models.py
@@ -7,7 +7,8 @@ import mimetypes
 def get_extensions_for_type(general_type):
     for ext in mimetypes.types_map:
         if mimetypes.types_map[ext].split('/')[0] == general_type:
-            yield ext
+            yield ext[1:]
+
 
 VIDEO = tuple(get_extensions_for_type('video'))
 
