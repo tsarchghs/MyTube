706585bd7205842dc41b26d32fc4aa82389c7e8c
gjergjk71 <gjergjk71@gmail.com>
2018-07-22 04:36:52 +0200

    Created createComment path

--- video/urls.py
+++ video/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
 	path("<video_id>",views.showVideo,name="showVideo"),
-	path("create",views.createVideo,name="createVideo"),
+	path("create/video",views.createVideo,name="createVideo"),
+	path("create/comment/<video_id>",views.createComment,name="createComment"),
 	path("edit/<video_id>",views.editVideo,name="editVideo"),
 	path("delete/<video_id>",views.deleteVideo,name="deleteVideo"),
 	path("delete/comment/<comment_id>",views.deleteComment,name="deleteComment"),
