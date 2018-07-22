c6c68c708d6a462cba1aaa2e0ddf077fda5182a0
gjergjk71 <gjergjk71@gmail.com>
2018-07-22 04:04:25 +0200

    Created likeVideo path

--- video/urls.py
+++ video/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
 	path("create",views.createVideo,name="createVideo"),
 	path("edit/<video_id>",views.editVideo,name="editVideo"),
 	path("delete/<video_id>",views.deleteVideo,name="deleteVideo"),
+	path("<type_>/video/<video_id>",views.likeVideo,name="likeVideo"),
 ]
