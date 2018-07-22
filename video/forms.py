de218f3aeea9a00ca79d7dd30a7e86e36b319ee4
gjergjk71 <gjergjk71@gmail.com>
2018-07-22 04:36:59 +0200

    Created CommentForm form

--- video/forms.py
+++ video/forms.py
@@ -5,3 +5,8 @@ class VideoForm(forms.ModelForm):
 	class Meta:
 		model = Video
 		exclude = ["channel"]
+
+class CommentForm(forms.ModelForm):
+	class Meta:
+		model = Comment
+		fields = ["content"]
