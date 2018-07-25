2943885020e005668a950b5be7a53b8489492745
gjergjk71 <gjergjk71@gmail.com>
2018-07-25 20:14:12 +0200

    Created showVideo view

--- channels/views.py
+++ channels/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-from .models import Channel
+from django.shortcuts import render,get_object_or_404
+from .models import Channel,Subscribe
+from video.models import Video
 
 # Create your views here.
 def index(request):
 	channels = Channel.objects.all()
 	return render(request, 'channels/index.html', {"channels":channels})
+
+def showChannel(request,channel_id):
+	channel = Channel.objects.get_object_or_404(Channel,pk=channel_id)
+	channel_videos = Video.objects.filter(channel=channel)
+	channel_subscribes = Subscribe.objects.filter(channel=channel)
+	context = {"channel":channel,
+			   "channel_videos":channel_videos,
+			   "channel_subscribes":channel_subscribes}
+	return render(request,"channels/showChannel.html",context)
