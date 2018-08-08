53f58520dc17fa59ecb38f3ce5209a1bea71705a
gjergjk71 <gjergjk71@gmail.com>
2018-08-08 20:19:13 +0200

    showVideo raises 404 if video object != found

--- video/views.py
+++ video/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect,reverse
+from django.shortcuts import render,redirect,reverse,get_object_or_404
 from channels.models import Channel
 from .models import Video,VideoLike,Comment,CommentLike,UserView,AnonymousView
 from .forms import VideoForm,CommentForm
@@ -20,7 +20,7 @@ def index(request):
 
 def showVideo(request,video_id):
 	user_agent = request.META['HTTP_USER_AGENT']
-	video = Video.objects.get(pk=video_id)
+	video = get_object_or_404(Video,pk=video_id)
 	if request.user.is_authenticated:
 		UserView.objects.create(user=request.user,browser=user_agent,video=video)
 	else:
