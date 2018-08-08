40cfd1d759ddfd9a0584d8c2e0245097fa2b31a8
gjergjk71 <gjergjk71@gmail.com>
2018-08-08 20:17:50 +0200

    Switched to explicit path variables

--- video/urls.py
+++ video/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 urlpatterns = [
 	path("",views.index,name="index"),
-	path("<video_id>",views.showVideo,name="showVideo"),
+	path("<int:video_id>",views.showVideo,name="showVideo"),
 	path("create/video",views.createVideo,name="createVideo"),
-	path("create/comment/<video_id>",views.createComment,name="createComment"),
-	path("edit/video/<video_id>",views.editVideo,name="editVideo"),
-	path("edit/comment/<comment_id>",views.editComment,name="editComment"),
-	path("delete/<video_id>",views.deleteVideo,name="deleteVideo"),
-	path("delete/comment/<comment_id>",views.deleteComment,name="deleteComment"),
-	path("<type_>/video/<video_id>",views.likeVideo,name="likeVideo"),
-	path("<type_>/comment/<comment_id>",views.likeComment,name="likeComment"),
+	path("create/comment/<int:video_id>",views.createComment,name="createComment"),
+	path("edit/video/<int:video_id>",views.editVideo,name="editVideo"),
+	path("edit/comment/<int:comment_id>",views.editComment,name="editComment"),
+	path("delete/<int:video_id>",views.deleteVideo,name="deleteVideo"),
+	path("delete/comment/<int:comment_id>",views.deleteComment,name="deleteComment"),
+	path("<str:type_>/video/<int:video_id>",views.likeVideo,name="likeVideo"),
+	path("<str:type_>/comment/<int:comment_id>",views.likeComment,name="likeComment"),
 ]
