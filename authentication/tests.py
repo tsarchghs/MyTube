10ddc847c2558c9e9dd23bddadc99fa25f1a0bb5
gjergjk71 <gjergjk71@gmail.com>
2018-08-09 14:55:15 +0200

    Created tests for login_view

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from django.shortcuts import reverse
 # Create your tests here.
+
+class TestViews(TestCase):
+	def setUp(self):
+		self.user_object = User.objects.create(username="testing",
+											   password="testing")
+	def test_custom_login_view(self):
+		url = reverse("login")
+		response_get = self.client.get(url)
+		self.assertEqual(response_get.status_code,200)
+		response_post = self.client.post(url,data=None)
+		self.assertEqual(response_post.status_code,200)
+		self.assertContains(response_post,'<form method="POST"')
+		"""		self.client.force_login(self.user_object)
+				response_get_loggedIn = self.client.get(url)
+				print(response_get_loggedIn)
+				self.assertEqual(response_get_loggedIn.status_code,302)
+				self.assertEqual(response_get_loggedIn.url,"/")
+		"""
