98c22d5f950de393ea12a7d1907c9ca19cd37fd7
gjergjk71 <gjergjk71@gmail.com>
2018-11-07 15:53:11 +0100

    Added DEFAULT_RENDERER_CLASSES

--- MyTube/settings.py
+++ MyTube/settings.py
@@ -37,7 +37,12 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    )
+
 }
 
 MIDDLEWARE = [
