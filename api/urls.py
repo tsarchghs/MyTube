c84a54f61fe205644f28c855d0c7d9a30fe95d98
gjergjk71 <gjergjk71@gmail.com>
2018-11-07 15:55:14 +0100

    Created ValidateCredentials path

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path,include
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
-
+	path("",include(router.urls)),
+	path("validate_credentials/",views.ValidateCredentials.as_view(),name="validate_credentials"),
 ]
