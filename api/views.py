6b44d7df1d725d2fab6976224c840d5c43bbe429
gjergjk71 <gjergjk71@gmail.com>
2018-11-07 15:54:56 +0100

    Created ValidateCredentials APIView

--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
 
 # Create your views here.
+
+class ValidateCredentials(APIView):
+	permission_classes = []
+	def get(self,request):
+		content = {"post_only":True}
+		return Response(content)
+	def post(self,request):
+		username = request.data.get("username")
+		password = request.data.get("password")
+		user = authenticate(username=username,password=password)
+		if user is not None:
+			content = {"valid_credentials":True}
+		else:
+			content = {"valid_credentials":False}
+		return Response(content)
