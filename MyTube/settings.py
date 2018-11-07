438230854d08789d174f52daca344917380a27d1
gjergjk71 <gjergjk71@gmail.com>
2018-11-07 15:05:24 +0100

    Config for read only access to unauthenticated users

--- MyTube/settings.py
+++ MyTube/settings.py
@@ -31,6 +31,14 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
