a67d3e752b7c36656f4f1deb7700477a9552fc8d
gjergjk71 <gjergjk71@gmail.com>
2018-11-08 22:16:44 +0100

    Removed unnessecary code

--- static/main.js
+++ static/main.js
@@ -2,18 +2,9 @@
 window.onload = function(e){
 	console.log(e);
 	authenticated_value = document.getElementById("authenticated").value;
-	logout_div = document.getElementById("logout_div");
-	signUp_signOut_div = document.getElementById("signUp_signOut_div");
 	login_button = document.getElementById("login_button");
 	login_form = document.getElementById("login_form");
 	login_button.onclick = login;
-	document.getElementById("logout_button").style.cssText = document.defaultView.getComputedStyle(login_button, "").cssText;
-	if (authenticated_value == "True"){
-		logout_div.innerHTML = "";
-	} else {
-		signUp_signOut_div.innerHTML = "";
-	}
-
 }
 
 window.addEventListener("keyup",(event) => {
