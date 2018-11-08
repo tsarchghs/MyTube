7b637d25465d245a36e631047f21ac2a9b1e0f34
gjergjk71 <gjergjk71@gmail.com>
2018-11-08 17:36:37 +0100

    Created UserProfile model

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class UserProfile(models.Model):
+	user = models.ForeignKey(User,on_delete=models.CASCADE)
+	photo = models.ImageField(upload_to="profile_photos/")
+	phone = models.IntegerField()
+	def __str__(self):
+		return self.user.username + " - profile"
