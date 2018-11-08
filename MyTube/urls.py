b187b8ad2e697cd14612419eb90fe9dfa8af6cef
gjergjk71 <gjergjk71@gmail.com>
2018-11-08 16:10:29 +0100

    Added namespace for api path

--- MyTube/urls.py
+++ MyTube/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
 	path('channel/', include('user_channel.urls')),
 	path('video/',include('video.urls')),
 	path('',include('home.urls')),
-	path('api/', include('api.urls')),
+	path('api/', include(('api.urls',"api"),namespace="api")),
 	path('api-auth/', include('rest_framework.urls')),
 ]
 
