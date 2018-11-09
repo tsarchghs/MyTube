e6f30a7724a9c88c77e11fa91ca06b5f6f0c0c77
gjergjk71 <gjergjk71@gmail.com>
2018-11-09 22:03:02 +0100

    Changed MEDIA_ROOT/URL values

--- MyTube/settings.py
+++ MyTube/settings.py
@@ -129,5 +129,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_media/')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_files/')
+MEDIA_URL = '/media_files/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media_files/')
