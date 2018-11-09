1404291aad952acb1f999566bd47254a6f68993b
gjergjk71 <gjergjk71@gmail.com>
2018-11-09 22:02:47 +0100

    Created showProfile view

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import UserProfile
 # Create your views here.
+
+
+def showProfile(request):
+	profile = UserProfile.objects.get(user=request.user)
+	context = {"user_profile":profile}
+	return render(request,"user_profile/profile.html",context)
