e3fd1303f11d9e5414d9108ba473b5ab64dea1eb
gjergjk71 <gjergjk71@gmail.com>
2018-11-10 13:51:25 +0100

    Registered ViewChannels to router

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ from . import views
 router = routers.DefaultRouter()
 router.register("users",views.ViewUsers)
 router.register("user_profiles",views.ViewUserProfiles)
+router.register("user_channels",views.ViewChannels)
 router.register("videos",views.ViewVideos)
 router.register("comments",views.ViewComments)
 
