bf379fdd88a304262b85d237aec2de546dcfcf02
gjergjk71 <gjergjk71@gmail.com>
2018-11-10 11:10:24 +0100

    Registered views to router

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,10 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
+router.register("users",views.ViewUsers)
+router.register("user_profiles",views.ViewUserProfiles)
+router.register("videos",views.ViewVideos)
+router.register("comments",views.ViewComments)
 
 urlpatterns = [
 	path("",include(router.urls)),
